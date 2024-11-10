964f0b92cb7cd62478db95e953751cc2e67b8a5f
michaellesteraguhob023 <michaellesteraguhob023@gmail.com>
2024-11-10 20:44:56 +0800

    Finished the project!

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ const cors = require('cors');
 
 const app = express();
 const corsOptions = ({
-    origin: ['https://blog-app-sepia-nine.vercel.app/', 'http://localhost:8000', 'http://localhost:3000'],
+    origin: ['https://blog-app-sepia-nine.vercel.app', 'http://localhost:8000', 'http://localhost:3000'],
     credentials: true,
     optionsSuccessStatus: 200
 })
